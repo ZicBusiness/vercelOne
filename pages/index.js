f1d6290586318287986d692970ccb72056f26617
ZicBusiness <zeroinfinitocriativo@gmail.com>
2024-02-02 12:51:13 -0200

    Orion up

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Orion lave suas mãos!</h1>
+    return <h1>Fanta Mistério da lata preta, foi lançada em 2021! Essa lata vermelha é da fanta místério 2023!</h1>
 }
 
 export default Home
