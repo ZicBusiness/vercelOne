c2e0cf7507aa938a282234b611ece5777c8048ee
ZicBusiness <zeroinfinitocriativo@gmail.com>
2024-02-03 17:28:03 -0200

    Next version

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const HomePage = () => {
     return (
       <header>
         <div style="background: linear-gradient(to right, violet, indigo,blue, green, yellow, orange, red); color: transparent; -webkit-background-clip: text; background-clip: text; font-size: 4.8em; padding: 10px; text-align: center;">-HARMONIQUE-</div>
-        <h1>Minha Página Next.js</h1>
+        <h1>Next.js</h1>
         <p>Bem-vindo ao meu projeto Next.js!</p>
       </header>
     );
