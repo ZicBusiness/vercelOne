8e1e1060b5bfeb05b6282b4b37621cde3c370d4f
ZicBusiness <zeroinfinitocriativo@gmail.com>
2024-02-03 19:44:48 -0200

    Css External

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import styles from '..style/styles.module.css';
+
+
 const HomePage = () => {
     return (
       <>
-        <h1 style={{ fontSize: '5em' }}>Quero isso maior</h1>
+        <h1 style={{ Size: '5em' }}>-Harmonique-</h1>
         <p>Bem-vindo ao meu projeto Next.js!</p>
       </>
     );
