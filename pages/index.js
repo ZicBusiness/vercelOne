c54732ba5112fa1395d969ccc7ef10ef59d50aa5
ZicBusiness <zeroinfinitocriativo@gmail.com>
2024-02-03 19:53:55 -0200

    Css External5

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import styles from '../style/styles.css';
-
 
 const HomePage = () => {
     return (
       <>
         <h1 style={{ Size: '5em' }}>-Harmonique-</h1>
-        <p>Bem-vindo ao meu projeto Next.js!</p>
+        <p style={{ Size: '5em' }}>Bem-vindo ao meu projeto Next.js!</p>
       </>
     );
   };
