ff21591ae9e79d20f19bcaa0474de13b9ef4a0ce
ZicBusiness <zeroinfinitocriativo@gmail.com>
2024-02-03 18:04:05 -0200

    Correction 4

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function HomePage() {
     return (
       <>
-        <h1>Quero isso maior</h1>
+        <h1 style="font-size: 5em;">Quero isso maior</h1>
         <p>Bem-vindo ao meu projeto Next.js!</p>
       </>
     );
