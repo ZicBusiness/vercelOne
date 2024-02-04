64ad7499ecb53a5e2a43df37006e669921c2e894
ZicBusiness <zeroinfinitocriativo@gmail.com>
2024-02-03 22:44:47 -0200

    New Style

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import styles from '.../styles/Home.module.css';
+
 const HomePage = () => {
     return (
       <>
         <h1 style={{ fontSize: '6em' }}>-Harmonique-</h1>
         <h2>Bem-vindo ao meu projeto Next.js!</h2>
+        <header>
+        <div className={styles.gradientText}>-HARMONIQUE-</div>
+      </header>
       </>
     );
   };
